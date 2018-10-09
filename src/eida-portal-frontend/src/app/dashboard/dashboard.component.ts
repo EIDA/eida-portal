b9f3ed12280c31b63e80e1a46013b47e65672f7a
Jarek Bienkowski <jarek@macjarek.knmi.nl>
2018-10-09 12:59:47 +0200

    Issue #4 WIP

--- src/eida-portal-frontend/src/app/dashboard/dashboard.component.ts
+++ src/eida-portal-frontend/src/app/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { ConsoleService } from '../console.service';
 
 declare var jquery: any;
 declare var $: any;
+declare var Mousetrap: any;
 
 @Component({
   selector: 'app-dashboard',
@@ -14,6 +15,34 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit() {
     this.consoleService.add('Dashboard initiated');
+
+    Mousetrap.bind('1', function(e) {
+      $('li').removeClass('is-active');
+      $('tab-pane').prop('checked', false);
+      $('#stationsTab').addClass('is-active');
+      $('#stations').prop('checked', true);
+    });
+
+    Mousetrap.bind('2', function(e) {
+      $('li').removeClass('is-active');
+      $('tab-pane').prop('checked', false);
+      $('#eventsTab').addClass('is-active');
+      $('#events').prop('checked', true);
+    });
+
+    Mousetrap.bind('3', function(e) {
+      $('li').removeClass('is-active');
+      $('tab-pane').prop('checked', false);
+      $('#requestTab').addClass('is-active');
+      $('#request').prop('checked', true);
+    });
+
+    Mousetrap.bind('4', function(e) {
+      $('li').removeClass('is-active');
+      $('tab-pane').prop('checked', false);
+      $('#consoleTab').addClass('is-active');
+      $('#console').prop('checked', true);
+    });
   }
 
   tabSelected(s) {
