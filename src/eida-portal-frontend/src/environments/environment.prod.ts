783531820cc35f722d3fe50460cb0b729897152a
Jarek Bienkowski <jarek@macjarek.knmi.nl>
2019-02-26 13:16:30 +0100

    Use HTTP for EMSC FDSN

--- src/eida-portal-frontend/src/environments/environment.prod.ts
+++ src/eida-portal-frontend/src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   networksUrl:  'https://www.orfeus-eu.org/epb/n',
   stationsUrl: 'https://www.orfeus-eu.org/epb/s',
   networksStationsUrl: 'https://www.orfeus-eu.org/epb/ns',
-  fdsnEventEmsc: 'https://seismicportal.eu/fdsnws/event/1/query?',
+  fdsnEventEmsc: 'http://seismicportal.eu/fdsnws/event/1/query?',
   fdsnEventIris: 'https://service.iris.edu/fdsnws/event/1/query?'
 };
