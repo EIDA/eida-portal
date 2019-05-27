b2d5899e3ead3039bcc09e879c66c75b45ffc90d
Jarek Bienkowski <jarek@macjarek.knmi.nl>
2019-05-27 12:56:13 +0200

    Added Federator URLs to the env

--- src/eida-portal-frontend/src/environments/environment.prod.ts
+++ src/eida-portal-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
+  federatorStationUrl: 'http://federator.orfeus-eu.org/fdsnws/station/1/query?',
+  federatorDataselectUrl: 'http://federator.orfeus-eu.org/fdsnws/dataselect/1/query?',
   networksUrl:  'https://www.orfeus-eu.org/epb/n',
   stationsUrl: 'https://www.orfeus-eu.org/epb/s',
   channelsUrl: 'https://www.orfeus-eu.org/epb/streams',
--- src/eida-portal-frontend/src/environments/environment.ts
+++ src/eida-portal-frontend/src/environments/environment.ts
@@ -7,6 +7,8 @@ export const environment = {
   // networksUrl:  'http://127.0.0.1:3000/n',
   // stationsUrl: 'http://127.0.0.1:3000/s',
   // channelsUrl: 'http://127.0.0.1:3000/streams',
+  federatorStationUrl: 'http://federator.orfeus-eu.org/fdsnws/station/1/query?',
+  federatorDataselectUrl: 'http://federator.orfeus-eu.org/fdsnws/dataselect/1/query?',
   networksUrl:  'https://www.orfeus-eu.org/epb/n',
   stationsUrl: 'https://www.orfeus-eu.org/epb/s',
   channelsUrl: 'https://www.orfeus-eu.org/epb/streams',
