ebe2baf69f5090828ca1139d8e5057708da2e04d
Jarosław Bieńkowski <jaroslaw.bienkowski@hotmail.com>
2019-07-10 12:57:04 +0000

    WIP #29 * Added backend CORS handler

--- src/eida-portal-backend/app/__init__.py
+++ src/eida-portal-backend/app/__init__.py
@@ -2,8 +2,10 @@ from flask import Flask
 from .config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
