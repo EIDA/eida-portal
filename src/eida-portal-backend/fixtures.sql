edd8dae3abc69d450e5489f1586ba871b2aaa549
Jarek Bienkowski <jaroslaw.bienkowski@hotmail.com>
2019-07-17 11:30:59 +0200

    Backend fixtures formatted

--- src/eida-portal-backend/fixtures.sql
+++ src/eida-portal-backend/fixtures.sql
@@ -1,10 +1,189 @@
-insert into nodes ("id", "code", "description", "url_base", "url_dataselect", "url_station", "url_routing", "url_wfcatalog") values ('1', 'ODC', 'ORFEUS Data Center', 'www.orfeus-eu.org', 'http://www.orfeus-eu.org/fdsnws/dataselect/1/query?', 'http://www.orfeus-eu.org/fdsnws/station/1/query?', 'http://www.orfeus-eu.org/eidaws/routing/1/query?', NULL);
-insert into nodes ("id", "code", "description", "url_base", "url_dataselect", "url_station", "url_routing", "url_wfcatalog") values ('2', 'GFZ', 'Helmholz-Zentrum Potsdam', 'geofon.gfz-potsdam.de', 'http://geofon.gfz-potsdam.de/fdsnws/dataselect/1/query?', 'http://geofon.gfz-potsdam.de/fdsnws/station/1/query?', NULL, NULL);
-insert into nodes ("id", "code", "description", "url_base", "url_dataselect", "url_station", "url_routing", "url_wfcatalog") values ('3', 'RESIF', 'Reseau Sismologique & Geodesique Francais', 'ws.resif.fr', 'http://ws.resif.fr/fdsnws/dataselect/1/query?', 'http://ws.resif.fr/fdsnws/station/1/query?', NULL, NULL);
-insert into nodes ("id", "code", "description", "url_base", "url_dataselect", "url_station", "url_routing", "url_wfcatalog") values ('4', 'INGV', 'Istituto Nazionale di Geofisica e Vulcanologia', 'webservices.ingv.it', 'http://webservices.ingv.it/fdsnws/dataselect/1/query?', 'http://webservices.ingv.it/fdsnws/station/1/query?', NULL, NULL);
-insert into nodes ("id", "code", "description", "url_base", "url_dataselect", "url_station", "url_routing", "url_wfcatalog") values ('5', 'ETHZ', 'ETH Zurich Schweizerischer Erdbebendienst (SED)', 'eida.ethz.ch', 'http://eida.ethz.ch/fdsnws/dataselect/1/query?', 'http://eida.ethz.ch/fdsnws/station/1/query?', NULL, NULL);
-insert into nodes ("id", "code", "description", "url_base", "url_dataselect", "url_station", "url_routing", "url_wfcatalog") values ('6', 'BGR', 'Bundesanstalt für Geowissenschaften und Rohstoffe', 'eida.bgr.de', 'http://eida.bgr.de/fdsnws/dataselect/1/query?', 'http://eida.bgr.de/fdsnws/station/1/query?', NULL, NULL);
-insert into nodes ("id", "code", "description", "url_base", "url_dataselect", "url_station", "url_routing", "url_wfcatalog") values ('7', 'NIEP', 'National Institute for Earth Physics', 'eida-sc3.infp.ro', 'http://eida-sc3.infp.ro/fdsnws/dataselect/1/query?', 'http://eida-sc3.infp.ro/fdsnws/station/1/query?', NULL, NULL);
-insert into nodes ("id", "code", "description", "url_base", "url_dataselect", "url_station", "url_routing", "url_wfcatalog") values ('8', 'KOERI', 'Kandilli Observatory and Earthquake Research Institute', 'eida-service.koeri.boun.edu.tr', 'http://eida-service.koeri.boun.edu.tr/fdsnws/dataselect/1/query?', 'http://eida-service.koeri.boun.edu.tr/fdsnws/station/1/query?', NULL, NULL);
-insert into nodes ("id", "code", "description", "url_base", "url_dataselect", "url_station", "url_routing", "url_wfcatalog") values ('9', 'LMU', 'Ludwig Maximilians Universitat Munchen', 'erde.geophysik.uni-muenchen.de', 'http://erde.geophysik.uni-muenchen.de/fdsnws/dataselect/1/query?', 'http://erde.geophysik.uni-muenchen.de/fdsnws/station/1/query?', NULL, NULL);
-insert into nodes ("id", "code", "description", "url_base", "url_dataselect", "url_station", "url_routing", "url_wfcatalog") values ('10', 'NOA', 'National Observatory of Athens', 'eida.gein.noa.gr', 'http://eida.gein.noa.gr/fdsnws/dataselect/1/query?', 'http://eida.gein.noa.gr/fdsnws/station/1/query?', NULL, NULL);
+insert
+	into
+		nodes ("id",
+		"code",
+		"description",
+		"url_base",
+		"url_dataselect",
+		"url_station",
+		"url_routing",
+		"url_wfcatalog")
+	values ('1',
+	'ODC',
+	'ORFEUS Data Center',
+	'www.orfeus-eu.org',
+	'http://www.orfeus-eu.org/fdsnws/dataselect/1/query?',
+	'http://www.orfeus-eu.org/fdsnws/station/1/query?',
+	'http://www.orfeus-eu.org/eidaws/routing/1/query?',
+	NULL);
+
+insert
+	into
+		nodes ("id",
+		"code",
+		"description",
+		"url_base",
+		"url_dataselect",
+		"url_station",
+		"url_routing",
+		"url_wfcatalog")
+	values ('2',
+	'GFZ',
+	'Helmholz-Zentrum Potsdam',
+	'geofon.gfz-potsdam.de',
+	'http://geofon.gfz-potsdam.de/fdsnws/dataselect/1/query?',
+	'http://geofon.gfz-potsdam.de/fdsnws/station/1/query?',
+	NULL,
+	NULL);
+
+insert
+	into
+		nodes ("id",
+		"code",
+		"description",
+		"url_base",
+		"url_dataselect",
+		"url_station",
+		"url_routing",
+		"url_wfcatalog")
+	values ('3',
+	'RESIF',
+	'Reseau Sismologique & Geodesique Francais',
+	'ws.resif.fr',
+	'http://ws.resif.fr/fdsnws/dataselect/1/query?',
+	'http://ws.resif.fr/fdsnws/station/1/query?',
+	NULL,
+	NULL);
+
+insert
+	into
+		nodes ("id",
+		"code",
+		"description",
+		"url_base",
+		"url_dataselect",
+		"url_station",
+		"url_routing",
+		"url_wfcatalog")
+	values ('4',
+	'INGV',
+	'Istituto Nazionale di Geofisica e Vulcanologia',
+	'webservices.ingv.it',
+	'http://webservices.ingv.it/fdsnws/dataselect/1/query?',
+	'http://webservices.ingv.it/fdsnws/station/1/query?',
+	NULL,
+	NULL);
+
+insert
+	into
+		nodes ("id",
+		"code",
+		"description",
+		"url_base",
+		"url_dataselect",
+		"url_station",
+		"url_routing",
+		"url_wfcatalog")
+	values ('5',
+	'ETHZ',
+	'ETH Zurich Schweizerischer Erdbebendienst (SED)',
+	'eida.ethz.ch',
+	'http://eida.ethz.ch/fdsnws/dataselect/1/query?',
+	'http://eida.ethz.ch/fdsnws/station/1/query?',
+	NULL,
+	NULL);
+
+insert
+	into
+		nodes ("id",
+		"code",
+		"description",
+		"url_base",
+		"url_dataselect",
+		"url_station",
+		"url_routing",
+		"url_wfcatalog")
+	values ('6',
+	'BGR',
+	'Bundesanstalt für Geowissenschaften und Rohstoffe',
+	'eida.bgr.de',
+	'http://eida.bgr.de/fdsnws/dataselect/1/query?',
+	'http://eida.bgr.de/fdsnws/station/1/query?',
+	NULL,
+	NULL);
+
+insert
+	into
+		nodes ("id",
+		"code",
+		"description",
+		"url_base",
+		"url_dataselect",
+		"url_station",
+		"url_routing",
+		"url_wfcatalog")
+	values ('7',
+	'NIEP',
+	'National Institute for Earth Physics',
+	'eida-sc3.infp.ro',
+	'http://eida-sc3.infp.ro/fdsnws/dataselect/1/query?',
+	'http://eida-sc3.infp.ro/fdsnws/station/1/query?',
+	NULL,
+	NULL);
+
+insert
+	into
+		nodes ("id",
+		"code",
+		"description",
+		"url_base",
+		"url_dataselect",
+		"url_station",
+		"url_routing",
+		"url_wfcatalog")
+	values ('8',
+	'KOERI',
+	'Kandilli Observatory and Earthquake Research Institute',
+	'eida-service.koeri.boun.edu.tr',
+	'http://eida-service.koeri.boun.edu.tr/fdsnws/dataselect/1/query?',
+	'http://eida-service.koeri.boun.edu.tr/fdsnws/station/1/query?',
+	NULL,
+	NULL);
+
+insert
+	into
+		nodes ("id",
+		"code",
+		"description",
+		"url_base",
+		"url_dataselect",
+		"url_station",
+		"url_routing",
+		"url_wfcatalog")
+	values ('9',
+	'LMU',
+	'Ludwig Maximilians Universitat Munchen',
+	'erde.geophysik.uni-muenchen.de',
+	'http://erde.geophysik.uni-muenchen.de/fdsnws/dataselect/1/query?',
+	'http://erde.geophysik.uni-muenchen.de/fdsnws/station/1/query?',
+	NULL,
+	NULL);
+
+insert
+	into
+		nodes ("id",
+		"code",
+		"description",
+		"url_base",
+		"url_dataselect",
+		"url_station",
+		"url_routing",
+		"url_wfcatalog")
+	values ('10',
+	'NOA',
+	'National Observatory of Athens',
+	'eida.gein.noa.gr',
+	'http://eida.gein.noa.gr/fdsnws/dataselect/1/query?',
+	'http://eida.gein.noa.gr/fdsnws/station/1/query?',
+	NULL,
+	NULL);
