b55602c49c148810d2c3de439dfb72f71ed80472
Jarosław Bieńkowski <jaroslaw.bienkowski@hotmail.com>
2019-09-06 10:54:54 +0000

    Simplified the routing config

--- src/eida-portal-frontend/src/app/app-routing.module.ts
+++ src/eida-portal-frontend/src/app/app-routing.module.ts
@@ -1,14 +1,11 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { DashboardComponent } from "./dashboard/dashboard.component";
 
-const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent }
-];
+const routes: Routes = [{ path: "", component: DashboardComponent }];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
