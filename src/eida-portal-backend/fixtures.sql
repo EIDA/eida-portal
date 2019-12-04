43f1d7a75dcd2a5f390df2f48c345374a4ca146d
Jarosław Bieńkowski <jaroslaw.bienkowski@hotmail.com>
2019-12-04 10:40:56 +0000

    Added UiB node to the config

--- src/eida-portal-backend/fixtures.sql
+++ src/eida-portal-backend/fixtures.sql
@@ -187,3 +187,22 @@ insert
 	'http://eida.gein.noa.gr/fdsnws/station/1/query?',
 	NULL,
 	NULL);
+
+insert
+	into
+		nodes ("node_id",
+		"node_code",
+		"node_description",
+		"node_url_base",
+		"node_url_dataselect",
+		"node_url_station",
+		"node_url_routing",
+		"node_url_wfcatalog")
+	values ('11',
+	'UiB',
+	'University of Bergen',
+	'eida.geo.uib.no',
+	'http://eida.geo.uib.no/fdsnws/dataselect/1/query?',
+	'http://eida.geo.uib.no/fdsnws/station/1/query?',
+	NULL,
+	NULL);
